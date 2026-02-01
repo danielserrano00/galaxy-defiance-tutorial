af27cc8351111f3313066610bf235860307d939c
Daniel Serrano <danielserrano00@gmail.com>
2026-02-01 16:12:20 -0500

    testing

--- player_ship/ship.gd
+++ player_ship/ship.gd
@@ -5,20 +5,21 @@ extends Node2D
 @onready var spawner_component: SpawnerComponent = $SpawnerComponent as SpawnerComponent
 @onready var fire_rate_timer: Timer = $FireRateTimer
 @onready var scale_component: ScaleComponent = $ScaleComponent as ScaleComponent
-@on
+@onready var move_component: MoveComponent = $MoveComponent as MoveComponent
+@onready var animated_sprite_2d: AnimatedSprite2D = $Anchor/AnimatedSprite2D
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	fire_rate_timer.timeout.connect(fire_lasers)
+    fire_rate_timer.timeout.connect(fire_lasers)
 
 func fire_lasers() -> void:
-	spawner_component.spawn(left_muzzle.global_position)
-	spawner_component.spawn(right_muzzle.global_position)
-	scale_component.tween_scale()
+    spawner_component.spawn(left_muzzle.global_position)
+    spawner_component.spawn(right_muzzle.global_position)
+    scale_component.tween_scale()
 
-func _process(delta: float) -> void:
-	animate_the_ship()
+func _process(_delta: float) -> void:
+    animate_the_ship()
 
 func animate_the_ship() -> void:
-	pass
+    pass
