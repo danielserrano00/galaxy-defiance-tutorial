33e687052a82543e6ee4056d851cd4706f4a4d65
Daniel Serrano <danielserrano00@gmail.com>
2026-02-01 22:55:36 -0500

    Added parallax background

---
+++ effects/space_background.gd
@@ -0,0 +1,11 @@
+extends ParallaxBackground
+
+@onready var space_layer: ParallaxLayer = %SpaceLayer
+@onready var far_stars_layer: ParallaxLayer = %FarStarsLayer
+@onready var close_stars_layer: ParallaxLayer = %CloseStarsLayer
+
+
+func _process(delta: float) -> void:
+	close_stars_layer.motion_offset.y += 15 * delta
+	far_stars_layer.motion_offset.y += 2 * delta
+	space_layer.motion_offset.y += 5 * delta
