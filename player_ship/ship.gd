a6cbff8153dd6a68018069f218f960d5429768eb
Daniel Serrano <danielserrano00@gmail.com>
2026-02-01 22:14:38 -0500

    added animations on player ship movement

--- player_ship/ship.gd
+++ player_ship/ship.gd
@@ -7,19 +7,28 @@ extends Node2D
 @onready var scale_component: ScaleComponent = $ScaleComponent as ScaleComponent
 @onready var move_component: MoveComponent = $MoveComponent as MoveComponent
 @onready var animated_sprite_2d: AnimatedSprite2D = $Anchor/AnimatedSprite2D
+@onready var flame_animated_sprite: AnimatedSprite2D = $Anchor/FlameAnimatedSprite
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-    fire_rate_timer.timeout.connect(fire_lasers)
+	fire_rate_timer.timeout.connect(fire_lasers)
 
 func fire_lasers() -> void:
-    spawner_component.spawn(left_muzzle.global_position)
-    spawner_component.spawn(right_muzzle.global_position)
-    scale_component.tween_scale()
+	spawner_component.spawn(left_muzzle.global_position)
+	spawner_component.spawn(right_muzzle.global_position)
+	scale_component.tween_scale()
 
 func _process(_delta: float) -> void:
-    animate_the_ship()
+	animate_the_ship()
 
 func animate_the_ship() -> void:
-    pass
+	if move_component.velocity.x < 0:
+		animated_sprite_2d.play("bank_left")
+		flame_animated_sprite.play("banking")
+	elif move_component.velocity.x > 0:
+		animated_sprite_2d.play("bank_right")
+		flame_animated_sprite.play("banking")
+	else:
+		animated_sprite_2d.play("center")
+		flame_animated_sprite.play("center")
